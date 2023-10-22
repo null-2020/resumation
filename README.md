6d22b453f31d161a58c1acdf238280e860b255b1
null-2020 <f1.siddiqui@yandex.com>
2023-10-22 00:11:24 -0400

    Updated files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# job-application-bot
+# Resumation
 
 Coded with python, selenium, and various other libraries, this bot uses a combination of webscraping, NLP, ML, with the help of ChatGPT and other APIS.
---
+++ lib/scraper.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+import sys

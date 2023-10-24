605feef4bc84ba991c006727c073d96296afcc5c
null-2020 <f1.siddiqui@yandex.com>
2023-10-24 01:23:43 -0400

    Added new files and modified existing files

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,8 +1,11 @@
-from time import sleep
+from pathlib import Path
+import json
 
+from time import sleep
+import random
+
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
-import os
-import sys
--- start.bat
+++ start.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 :: This file only works on Windows
 TITLE Flask App
+ECHO Starting Flask App
 flask --app app run

7b2d8cb6e89e383ff5d3c0418e78b21c97ad87e7
null-2020 <f1.siddiqui@yandex.com>
2023-11-03 01:08:45 -0400

    Updated start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-echo "Starting app"
-flask --app app run --debug
+if [[ $1 == "debug" ]]
+then
+  echo "Starting app in debug mode"
+  flask --app app run --debug &
+  npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css --watch
+else
+  echo "Starting app"
+  flask --app app run &
+  npx tailwindcss -i ./static/src/input.css -o ./static/dist/css/output.css --watch
+fi
